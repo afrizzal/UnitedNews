fac5aa9b3ca1045c4c6319b0b92c9084c7eb1e44
Afrizzal P. Pratama <apratama8@gmail.com>
2021-07-13 14:34:23 +0700

    Initial Commit

---
+++ UnitedNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitedNews
+//
+//  Created by Afrizzal on 13/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title="News"
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
