118b342e9d7ef5e5429df512f9a50e5737b933f5
Afrizzal P. Pratama <apratama8@gmail.com>
2021-07-14 07:27:46 +0700

    GET API Endpoints from apinews.org

---
+++ UnitedNews/APICaller.swift
@@ -0,0 +1,57 @@
+//
+//  APICaller.swift
+//  UnitedNews
+//
+//  Created by Afrizzal on 13/07/21.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=af3d0d8a78734d1dae2c8b1f55f947ea")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[String], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        let task =  URLSession.shared.dataTask(with: url) {data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    print("Articles: \(result.articles.count)")
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+// Models
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+    
+struct Source: Codable {
+    let name: String
+    }
--- UnitedNews/ViewController.swift
+++ UnitedNews/ViewController.swift
@@ -6,15 +6,58 @@
 //
 
 import UIKit
+// TableView
+// Custom Cell
+// API Caller
+// Open the News Story
+// Search for News Story
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title="News"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .systemBackground
+        
+        APICaller.shared.getTopStories {result in
+        switch result {
+        case .success(let response):
+            break
+        case .failure(let error):
+            print(error)
+            }
+        }
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    //Table
+    func tableView(_ tableView: UITableView, NumberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITbaleViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "cell",
+            for: IndexPath
+        )
+        cell.textLabel?.text = "Something"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: <#T##IndexPath#>, animated: true)
+    }
 }
 
